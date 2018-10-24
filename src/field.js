92ce60e9842eb130dd46505e48622ae8ee515811
Jhonatan Sabadi <jhonatan.sabadi@icnavais.com>
2018-10-24 07:11:30 -0300

    Set mapStateToProps

--- src/field.js
+++ src/field.js
@@ -1,30 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Field extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value: props.initialValue
-        }
-
-        this.handleChange = this.handleChange.bind(this);
-
-    }
-
-    handleChange(event){
-        this.setState({
-            value: event.target.value
-        })
-    }
-
-    render(){
+   render(){
         return (
             <div>
-                &nbsp;<label>{this.state.value}</label> <br /><br />
-                <input onChange={this.handleChange} value={this.state.value} />
+                &nbsp;<label>{this.props.value}</label> <br /><br />
+                <input onChange={this.handleChange} value={this.props.value} />
             </div>
         )
     }
 }
 
-export default Field;
+function mapStateToProps(state){
+    return {
+        value: state.field.value
+    }
+}
+
+export default connect(mapStateToProps)(Field);
