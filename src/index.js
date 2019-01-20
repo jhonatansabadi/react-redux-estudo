97e866bf40e17e855ecaaf58ff5fb5cf5004ab24
jhonatansabadi <jhonatansabadi@hotmail.com>
2019-01-20 00:05:26 -0200

    Adicção de componente

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
-import { Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { createBrowserHistory  } from 'history';
 
 import Main from './modules/Main';
